415b9ddc0523bf9cc297787a1001a417ed98cad2
kaa-dan <nithinraj.dev@gmail.com>
2025-04-17 10:26:29 +0530

    question one

--- random/question1.cpp
+++ random/question1.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
 
-int main()
+class Solution
 {
-    std::cout << " Hey i am Nithin ";
-    return 0;
-}
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int complement = target - nums[i];
+            if (map.find(complement) != map.end())
+            {
+                return {map[complement], i};
+            }
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+
+    vector<int> twoSum2(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> indices;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (indices.count(target - nums[i]))
+            {
+                return {indices[target - nums[i]], i};
+            }
+            indices[nums[i]] = i;
+        }
+        return {};
+    }
+};
